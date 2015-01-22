9a15818aa858a7e64e53fff2f35314a8ac4ecd58
yao cai <yao@yaos-MacBook-Pro.local>
2015-01-22 10:44:25 -0800

    Completed Year.java

--- lab1/Year.java
+++ lab1/Year.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Yao Cai
  */
 public class Year {
 
@@ -8,9 +8,12 @@ public class Year {
      *          false if year is not a leap year
      */
     static boolean isLeapYear(int year) {
-        return true;    // YOUR CODE HERE
-    }
-
+        if (year%400==0 || (year%4==0 && year%100 !=0)){
+        return true;
+     } else {
+	return false;
+}
+}
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
