64256e45f951204acbd6499dd3445bb2a0360929
yao cai <yao@yaos-MacBook-Pro.local>
2015-02-03 14:44:15 -0800

    try to pass test 3

--- hw2/CalculatorTest.java
+++ hw2/CalculatorTest.java
@@ -26,35 +26,16 @@ public class CalculatorTest {
     @Test
     public void addTest1(){
     	assertEquals(18824185,tester.add(18735426,88759));
+        assertEquals(0,tester.add(10,-10));
+        assertEquals(-108,tester.add(3,-111));
     }
-
-    @Test
-    public void addTest2(){
-    	assertEquals(0,tester.add(10,-10));
-    }
-
-    @Test
-    public void addTest3(){
-    	assertEquals(-108,tester.add(3,-111));
-    }
+   
 
     @Test
     public void multiplyTest1(){
     	assertEquals(64449,tester.multiply(189,341));
-    }
-
-    @Test
-    public void multiplyTest2(){
     	assertEquals(0,tester.multiply(0,-10));
-    }
-
-    @Test
-    public void multiplyTest3(){
     	assertEquals(3774,tester.multiply(-34,-111));
-    }
-
-    @Test
-    public void multiplyTest4(){
     	assertEquals(-3774,tester.multiply(34,-111));
     }
 
