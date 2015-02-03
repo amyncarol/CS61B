d7385bb5d9ce92c205f2f842d07a065ae78aba36
yao cai <yao@yaos-MacBook-Pro.local>
2015-02-03 15:04:08 -0800

    deleted extra tests

--- hw2/CalculatorTest.java
+++ hw2/CalculatorTest.java
@@ -26,7 +26,15 @@ public class CalculatorTest {
     @Test
     public void addTest1(){
     	assertEquals(18824185,tester.add(18735426,88759));
+    }
+
+    @Test
+    public void addTest2(){
         assertEquals(0,tester.add(10,-10));
+    }
+
+    @Test
+    public void addTest3(){   
         assertEquals(-108,tester.add(3,-111));
     }
    
@@ -34,13 +42,25 @@ public class CalculatorTest {
     @Test
     public void multiplyTest1(){
     	assertEquals(64449,tester.multiply(189,341));
+    }
+   
+
+    @Test
+    public void multiplyTest2(){
     	assertEquals(0,tester.multiply(0,-10));
+    }
+   
+    @Test
+    public void multiplyTest3(){
     	assertEquals(3774,tester.multiply(-34,-111));
+    }
+
+    @Test
+    public void multiplyTest4(){
     	assertEquals(-3774,tester.multiply(34,-111));
     }
 
-
- 	@Test
+ 	/*@Test
  	public void saveEquationTest(){
  		EquationList temp = tester.eqList;
  		tester.saveEquation("1 + 2",3);
@@ -49,10 +69,7 @@ public class CalculatorTest {
  		assertEquals(temp,tester.eqList.next);
  	}
 
- 	@Test
- 	public void printHistoryTest(){
- 		tester.printHistory(3);
- 	}
+ 	
     /* Run the unit tests in this file. */
     public static void main(String... args) {
         jh61b.junit.textui.runClasses(CalculatorTest.class);
