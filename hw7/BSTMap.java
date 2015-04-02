f3d8d9d331e44d917eea85a3811686a96923a442
yao cai <yao@yaos-MacBook-Pro.local>
2015-04-01 19:28:22 -0700

    compile problem

--- hw7/BSTMap.java
+++ hw7/BSTMap.java
@@ -1,8 +1,56 @@
+import java.util.Set;
+
 public class BSTMap implements Map61B<Integer, Integer> {
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean containsKey(Integer key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Integer get(Integer key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void put(Integer key, Integer value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Integer remove(Integer key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Integer remove(Integer key, Integer value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 	
 
-	public static void main(String[] args){
-
-	}
+	
 }
 	
